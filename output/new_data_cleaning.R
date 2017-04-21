3750cfe2c74e9fbe9a3a3f746b4718af3dc2ae9e
Kai Chen <kc3041@columbia.edu>
2017-04-21 19:20:40 -0400

    new

---
+++ output/new_data_cleaning.R
@@ -0,0 +1,73 @@
+setwd("../output")
+library(readr)
+bg=read_csv('../data/background.csv')
+train=read_csv('../data/train.csv')
+
+# Delete the records that corresponding to NA in the train
+naid<- which(is.na(train$gpa))
+bgtrain<- bg[setdiff(train$challengeID,train$challengeID[naid]),]
+
+# Delete the variables that are 80% NAs
+n <- ncol(bgtrain)
+na_count<- rep(NA, n)
+for(i in 1:n){
+  na_count[i]<- sum(is.na(bgtrain[,i]))
+}
+na_index<- c(1:n)[na_count>=0.8*nrow(bgtrain)]
+bgtrain<- bgtrain[,-na_index]
+
+# Delete the variables that have the same value
+namelist=names(bgtrain)
+for (f in namelist) {
+  if(nrow(unique(bgtrain[f]))==1) {
+    bgtrain[f]=NULL
+  }
+}
+
+# Delete the variables that can neither be used as charater 
+n<- ncol(bgtrain)
+t<- rep(NA, n)
+for(i in 1:n){
+  t[i]<- typeof(bgtrain[[1,i]])
+}
+# unique(t)
+char_index<- c(1:n)[t=="character"]
+inter_index<- c(1:n)[t=="integer"]
+dou_index<- c(1:n)[t=="double"]
+
+# seperate the variable by different type, charater are those that cannot
+# be convert to any type of data, doubles are continuous data. And intergers may
+# contain part of continuous data. Thus, we should also handle these variables.
+bgtrain_char<- bgtrain[, char_index]
+bgtrain_inter<- bgtrain[, inter_index]
+bgtrain_dou<- bgtrain[, dou_index]
+
+
+### fill NA and create new data
+fill_each_column <- function(each_col){
+  na_label <- is.na(each_col)
+  if(sum(na_label) > 0){
+    cate_col <- ifelse(na_label == T, 1, 0) ## T = 1, is NA
+    fill <- median(each_col, na.rm = T)
+    each_col[na_label] <- fill
+    return(list(NEW_COLUMN = each_col, NEW_CATE = cate_col))
+  }else{
+    ## no NA in a column
+    return(list(NEW_COLUMN = rep(0,length(each_col)), NEW_CATE = rep(0,length(each_col))))
+  }
+  
+  
+}
+#fill_each_column(gp5data_dou[,839])
+
+
+## INPUT data= continuous dataframe 
+gp5data_dou <- bgtrain_dou
+gp5data_dou <- matrix(unlist(gp5data_dou), nrow(gp5data_dou))
+## 1 for new column, 2 for new categorical features
+gp5data_dou_RMNA <- apply(gp5data_dou, 2, function(col){fill_each_column(col)[[1]]})
+cate_dou_na <- apply(gp5data_dou, 2, function(col){fill_each_column(col)[[2]]})
+
+## remove NON-NA columns
+non_na_dou <- colSums(cate_dou_na) == 0
+cate_dou_na <- cate_dou_na[,!non_na_dou]
