7c1ca90d6f65a861b0c2db31c48d6d52e1a59095
zf2169 <zf2169@columbia.edu>
2017-04-26 17:12:02 -0400

    clean code

--- lib/gpa_model_2.R
+++ lib/gpa_model_2.R
@@ -1,7 +1,13 @@
-setwd("../output")
+
+### library 
 library(readr)
-bg=read_csv('../data/background.csv')
-train=read_csv('../data/train.csv')
+library(Matrix)
+library(mlr)
+library(xgboost)
+library(plyr)
+
+bg=read_csv('background.csv')
+train=read_csv('train.csv')
 
 # Delete the records that corresponding to NA in the train
 naid<- which(is.na(train$gpa))
@@ -47,7 +53,7 @@ bgtrain_dou<- bgtrain[, dou_index]
 # more than 20 factors as continuous number.
 fac_num<- as.numeric(apply(bgtrain_inter, 2, function(vec) 
 {return(length(table(vec)))} ))
-fac_index<- which(fac_num<=20)
+fac_index<- which(fac_num<=15)
 bgtrain_dou<- cbind(bgtrain_inter[,-fac_index], bgtrain_dou)
 bgtrain_inter<- bgtrain_inter[,fac_index]
 
@@ -85,10 +91,26 @@ cate_dou_na <- apply(gp5data_dou, 2, function(col){fill_each_column(col)[[2]]})
 ## remove NON-NA columns
 non_na_dou <- colSums(cate_dou_na) == 0
 cate_dou_na <- cate_dou_na[,!non_na_dou]
+# 
+# ####### K-MEANS func
+# 
+new_features_kmeans <- function(data, K){
+  ## Return cluster.id
+  kmeans_results <- kmeans(data, centers = K, iter.max = 100)
+  return(kmeans_results$cluster)
+}
+# 
+generate_new_f_kmeans <- function(data, cluster.id){
+  new_data_lm <- data.frame(CLUS = cluster.id,
+                            Records = data)
+  new_data_lm_done <- ddply((new_data_lm), .(CLUS), colMeans)
+  return(DATA = t(new_data_lm_done[,-1]))
+}
 
-dim(bg)
+
+# dim(bg)
 bg_dou <- bg[,colnames(bgtrain_dou)]
-dim(bg_dou)
+# dim(bg_dou)
 
 gp5pred_dou <- bg_dou
 gp5pred_dou <- matrix(unlist(gp5pred_dou), nrow(gp5pred_dou))
@@ -96,7 +118,83 @@ gp5pred_dou <- matrix(unlist(gp5pred_dou), nrow(gp5pred_dou))
 gp5pred_dou_RMNA <- apply(gp5pred_dou, 2, function(col){fill_each_column(col)[[1]]})
 cate_pred_dou_na <- apply(gp5pred_dou, 2, function(col){fill_each_column(col)[[2]]})
 
-## remove NON-NA columns
-non_na_dou2 <- colSums(cate_pred_dou_na) == 0
-cate_pred_dou_na <- cate_pred_dou_na[,!non_na_dou2]
+my.text <- "^([a-z]{1,3}5)"
+indices <- grepl(my.text, colnames(bgtrain_inter))
+mat <- bgtrain_inter[,indices]
+
+
+#grp5_cate_422 <- bgtrain_inter
+grp5_cate_422 <- mat
+grp5_cate_422[is.na(grp5_cate_422)] <- -32767
+for (iter in 1:ncol(grp5_cate_422 )){
+  grp5_cate_422[,iter] <- as.factor(grp5_cate_422[,iter])
+}
+aaa <- apply(grp5_cate_422, 2, createDummyFeatures)
+
+aaa1 <- matrix(ncol = 0, nrow = nrow(grp5_cate_422))
+for (iter in 1:length(aaa)){
+  aaa1 <- cbind(aaa1, aaa[[iter]][,2:ncol(aaa[[iter]])])
+}
+
+cs.id <- new_features_kmeans(t(aaa1), K = 120)
+new_features_train <- generate_new_f_kmeans(t(aaa1), cs.id)
+
+
+
+
+## bg_dou_cate begin
+
+
+#bg_inter <- bg[, colnames(bgtrain_inter)]
+bg_inter <- bg[, colnames(mat)]
+grp5_pre_cate_422 <- bg_inter
+grp5_pre_cate_422[is.na(grp5_pre_cate_422)] <- -32767
+grp5_pre_cate_422 <- data.frame(grp5_pre_cate_422)
+
+
+#### fill in the values
+for (iter in 1:ncol(grp5_pre_cate_422 )){
+  set1 <- colnames(aaa[[iter]])
+  grp5_pre_cate_422[!(grp5_pre_cate_422[,iter] %in% as.numeric(set1)),iter] <- -32767
+  grp5_pre_cate_422[,iter] <- as.factor(grp5_pre_cate_422[,iter])
+}
+
+aaa_pre <- apply(grp5_pre_cate_422, 2, createDummyFeatures) 
+## This is a list
+aaa_pre1 <- matrix(ncol = 0, nrow = nrow(grp5_pre_cate_422))
+for (iter in 1:length(aaa_pre)){
+  aaa_temp <- aaa_pre[[iter]][,colnames(aaa[[iter]])]
+  aaa_temp <- aaa_temp[,2:ncol(aaa[[iter]])]
+  aaa_pre1 <- cbind(aaa_pre1, aaa_temp)
+}
+
+dim(aaa_pre1)
+dim(aaa1)
+## k-means features
+new_features_test <- generate_new_f_kmeans(t(aaa_pre1), cs.id)
+
+## XGBOOST FEATURES
+dtrain=xgb.DMatrix(data.matrix(new_features_train),label=train$gpa[which(!is.na(train$gpa))])
+dtest=xgb.DMatrix(data.matrix(new_features_test))
+#dim(dtrain)
+#dim(dtest)
+
+params=list(
+  objective='reg:linear',
+  subsample=0.9,
+  colsample_bytree=1,  ## This can be modified 
+  eta=0.05,
+  max_depth=1
+)
+
+
+xgbbb <- xgb.cv(nfold=10,data=dtrain,params = params,nround=300,print_every_n = 50)
+model8=xgb.train(data=dtrain,params=params,nrounds=140)
+#imp=xgb.importance(model=model8)
+
+cate_result <- predict(model8,dtest)
+model2 <- read_csv('prediction.csv')
+model2$gpa <- cate_result
+write_csv(model2,'model2.csv')
+
 
